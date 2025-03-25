2fbe9f7dc64932dd9633a9f3391daa0d8fd6d4fd
Deep2106 <Deep2106@users.noreply.github.com>
2025-03-25 23:06:10 +0000

    Update submit_susie_batch.sh

--- submit_susie_batch.sh
+++ submit_susie_batch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # === SETTINGS ===
-total_genes=16915
-batch_size=900
+total_genes=20000 ## change as per your data
+batch_size=1000 ## change as per your data
 r_script="run_susie_batch.R"
 log_dir="logs"
 mkdir -p "$log_dir"
@@ -19,7 +19,7 @@ for ((start=1; start<=total_genes; start+=batch_size)); do
 #SBATCH -N 1
 #SBATCH -p compute
 #SBATCH -J FM_${start}_${end}
-#SBATCH --mem=15G
+#SBATCH --mem=12G
 #SBATCH -c 2
 #SBATCH -t 96:00:00
 #SBATCH -o "${log_dir}/SuSiE_${start}_${end}_%j.out"
@@ -31,9 +31,9 @@ export MKL_NUM_THREADS=2
 export NUMEXPR_NUM_THREADS=2
 export VECLIB_MAXIMUM_THREADS=2
 export R_BLAS_USE_FLOCK=TRUE
-
-cd /home/shared/Deepak/EQTL/SNPDATA_COVID19_LOCAL_UU/EQTL/TMM_log2 ;
-
+#######################################################
+cd ~/EQTL/TMM_log2 ; ### change as per your folder strutures
+#######################################################
 echo "Starting SuSiE batch: genes ${start} to ${end}"
 Rscript "$r_script" "$start" "$end"
 echo "Finished SuSiE batch: genes ${start} to ${end}"
